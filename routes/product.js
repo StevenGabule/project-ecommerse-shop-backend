9a011e88a37c55f4bfaff335f46801ae802a3a84
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-03-15 01:17:39 +0800

    handle authentication like login and register

--- routes/product.js
+++ routes/product.js
@@ -1,29 +1,9 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-import Product from "../models/product.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 const router = express.Router();
-
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get("/:id", async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      return res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found!");
-    }
-  } catch (error) {
-    console.error(`${error.message}`);
-    res.status(500).send(error.message);
-  }
-});
-
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 export default router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import product from "./routes/product.js";
+import user from "./routes/users.js";
 import { errorHandler, notFound } from "./middleware/error.js";
 
 dotenv.config();
@@ -11,11 +12,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("WELCOME TO API MERN STACK!");
 });
 
 app.use("/api/products", product);
+app.use("/api/users", user);
 
 app.use(notFound);
 
