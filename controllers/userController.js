7f409a01d7c9e3ae6f466587cca2776563db905e
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-07-14 00:17:25 +0800

    feature: online payment using paypal - checkout

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,97 +6,101 @@ import generateToken from "../utils/generateToken.js";
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+    const {email, password} = req.body;
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({email});
 
-  if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(401);
-    throw new Error("Invalid email or password");
-  }
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401).json({error: "Invalid email or password"});
+    }
 });
 
 // @desc    Register user
 // @route   POST /api/users/register
 // @access  Public
-const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+const registerUser = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
 
-  const userExists = await User.findOne({ email });
+        if (name === "" || email === "" || password === "") {
+            return res.status(400).json({error: "All fields are required!"});
+        }
 
-  if (userExists) {
-    res.status(400);
-    throw new Error("User already exists, Please used another active email");
-  }
+        const userExists = await User.findOne({email});
 
-  const user = await User.create({
-    name,
-    email,
-    password,
-  });
-  if (user) {
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(400);
-    throw new Error("Invalid user data");
-  }
-});
+        if (userExists) {
+            return res.status(400).json({error: "User already exists, Please used another active email"});
+        }
+
+        const user = await User.create({
+            name,
+            email,
+            password,
+        });
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
+
+    } catch (e) {
+        res.status(400).json({error: e.message})
+    }
+};
 
 // @desc    Auth user && get token
 // @route   POST /api/users/login
 // @access  Public
 const updateUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
-  if (user) {
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
-    if (req.body.password) {
-      user.password = req.body.password;
+    const user = await User.findById(req.user._id);
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id),
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found!");
     }
-
-    const updatedUser = await user.save();
-
-    res.json({
-      _id: updatedUser._id,
-      name: updatedUser.name,
-      email: updatedUser.email,
-      isAdmin: updatedUser.isAdmin,
-      token: generateToken(updatedUser._id),
-    });
-  } else {
-    res.status(404);
-    throw new Error("User not found!");
-  }
 });
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
-  if (user) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(404);
-    throw new Error("User not found!");
-  }
+    const user = await User.findById(req.user._id);
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found!");
+    }
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+export {authUser, getUserProfile, registerUser, updateUserProfile};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import colors from "colors";
 import connectDB from "./config/db.js";
 import product from "./routes/product.js";
 import user from "./routes/users.js";
+import order from "./routes/order.js";
 import { errorHandler, notFound } from "./middleware/error.js";
 
 dotenv.config();
@@ -13,21 +14,21 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("WELCOME TO API MERN STACK!");
-});
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/products", product);
 app.use("/api/users", user);
+app.use("/api/orders", order);
+
+app.get("/api/config/paypal", (req, res) => {
+  return res.send(process.env.PAYPAL_CLIENT_ID);
+});
 
 app.use(notFound);
-
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running in port: ${PORT}`.yellow.inverse.bold);
-  // console.log(`Server is running in port: ${PORT}`);
 });
